1d7cbd93c1440e8c28096fae06d1f396109ddaec
Nuriia <sheripova.n.ii.a@gmail.com>
2022-10-25 11:29:08 +0600

    init:start

---
+++ 21.10.tasks/21.10.tasks.js
@@ -0,0 +1,4 @@
+let user_name1 = 'Niia';
+let user_name2 = 'Sheripova';
+user_name1 = username2;
+alert(user_name2);
---
+++ t25.10/script.js
@@ -0,0 +1,25 @@
+/*************************1******************/
+/*let a = 17;
+let b = 10;
+console.log(a - b);
+let result = c;*/
+
+let a = 17;
+let b = 10;
+let c = a-b;
+console.log(c);
+
+let d = 7;
+let result = c +d;
+console.log(result);
+
+/*************************2******************/
+let hour = 10;
+let minute = 50;
+let second = 59;
+let time = `${hour}:${minute}:${second}`;
+console.log(time);
+/*************************3******************/
+let num = 10;
+let k = num++;
+console.log(num);
