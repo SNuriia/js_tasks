d963a3c92bdb8b42911654bbbff2deac7bd9cb27
Nuriia <sheripova.n.ii.a@gmail.com>
2022-10-29 20:20:44 +0600

    feat: HW28/10

---
+++ t28.10/script.js
@@ -0,0 +1,36 @@
+/**********      1     **********/
+/*let test = prompt('Сколько равняется х ?');
+if(test === '7'){
+    alert('Верно!');
+} else{
+    alert('Неверно!');
+}*/
+
+/**********      2    **********/
+/*let numa =10;
+let numb=3;
+if(numa <= 1 && numb >=3 ){
+    alert(numa + numb)
+} else{
+    alert(numa - numb)
+}*/
+/**********      3    **********/
+let time = prompt('Введите число ?');
+if( time >=0 && time <15) {
+    alert('Первая четверть')
+} else if( time >=15 && time <30){
+    alert('2 четверть')
+} else if( time >=30 && time <45){
+    alert('3 четверть')
+}  else if( time >=45 && time <60){
+    alert('4 четверть')
+} else{
+    alert('Значение не попадает в диапазон от 0 до 59')
+}
+
+
+
+
+
+
+
